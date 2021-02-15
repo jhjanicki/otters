dd41bd81da2725a435849c04c1a1ff46b453710e
Julia <>
2021-02-15 14:14:04 +0100

    added spp ranges

---
+++ profilesData.js
@@ -0,0 +1,38 @@
+var profilesData =[
+{
+"id": 1,
+"species":"Aonyx cinereus",
+"name":"Asian Small-clawed Otter",
+"description":"",
+"range":"",
+"status":"VU",
+"img":"./img/img1.jpg"
+},
+{
+"id": 2,
+"species":"Lutra lutra",
+"name":"Eurasian otter",
+"description":"",
+"range":"",
+"status":"NT",
+"img":"./img/img2.jpg"
+},
+{
+"id": 3,
+"species":"Lutra sumatrana",
+"name":"Hairy-nosed Otter",
+"description":"",
+"range":"",
+"status":"EN",
+"img":"./img/img3.jpg"
+},
+{
+"id": 4,
+"species":"Lutrogale perspicillata",
+"name":"Smooth-coated Otter",
+"description":"",
+"range":"",
+"status":"VU",
+"img":"./img/img4.jpg"
+}
+]
